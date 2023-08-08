7282b7a0cf07b1ca21d3994485d8678a8942fcd1
quangmta <nguyennhuquang1998@gmail.com>
2023-08-08 21:16:14 +0300

    Auto crop panorama

--- crop_panorama.py
+++ crop_panorama.py
@@ -1,9 +1,58 @@
 import numpy as np
 import cv2
+
 folder = '27072023-1628'
 width = int(3280/2464*384/62.2*360)
 panorama = cv2.imread("output/panorama/"+folder+"-panorama.png")
-panorama = cv2.resize(panorama,(width,384))
-cv2.imwrite("output/panorama/"+folder+".png",panorama[20:-20,:-2,:])
-cv2.imshow("img",panorama[20:-20,:-2,:])
+print(panorama.shape)
+
+def calculate_dark(image):
+    for j in range(0,image.shape[1]):
+        check = False
+        for i in range(100,image.shape[0]-100):
+            if np.array_equal(image[i,j],[0,0,0]):
+                check = True
+                break
+        if i == image.shape[0]-101 and check == False:
+            break
+    left = j
+    print(left)
+    
+    for j in range(image.shape[1]-1,image.shape[1]-100,-1):
+        check = False
+        for i in range(100,image.shape[0]-100):
+            if np.array_equal(image[i,j],[0,0,0]):
+                check = True
+                break
+        if i == image.shape[0]-101 and check == False:
+            break
+    right = j+1
+    print(right)
+        
+    for i in range(0,100):
+        check = False
+        for j in range(left,right):
+            if np.array_equal(panorama[i,j],[0,0,0]):
+                check = True
+                break
+        if j == right-1 and check == False:
+            break
+    top = i  
+    print(top) 
+
+    for i in range(image.shape[0]-1,image.shape[0]-101,-1):
+        check = False
+        for j in range(left,right):
+            if np.array_equal(panorama[i,j],[0,0,0]):
+                break
+        if j == right-1 and check == False:
+            break
+    bottom = i
+    print(bottom)
+    return panorama[top:bottom,left:right]
+
+panorama = calculate_dark(panorama)
+panorama = cv2.resize(panorama,(width,384),cv2.INTER_AREA)
+cv2.imwrite("output/panorama/"+folder+".png",panorama)
+cv2.imshow("img",panorama)
 cv2.waitKey(0)
