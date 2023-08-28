8162d66d3169a6a656217e5053fda9f37af5bed5
quangmta <nguyennhuquang1998@gmail.com>
2023-08-28 17:56:07 +0300

    ignore

--- test_size.c
+++ test_size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-# pragma pack (16)
-struct test_size
-{
-    char a;
-    int b;
-};
+// # pragma pack (16)
+// struct test_size
+// {
+//     char a;
+//     int b;
+// };
 int main()
 {
-    printf("size = %d",sizeof(struct test_size));
+    // printf("size = %d",sizeof(struct test_size));
     return 0;
 }
